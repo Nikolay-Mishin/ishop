2a8e008cc7c56201d42f08b584cf903ea0ea9d4e
Николай Мишин <mishin.nikolay.d3@mail.ru>
2019-09-20 14:14:29 +0300

    New files 2-5

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -1,11 +1,11 @@
 <?php
-// базовый класс Контроллеров приложения, который наследуют остальные контроллеры
-// extends (наследует базовый класс контроллера фреймворка) - AppController наследует класс Controller (расширяет/дополняет его)
 
 namespace app\controllers;
 
 use app\models\AppModel; // подключаем базовый класс Моделей приложения
 use ishop\base\Controller; // подключаем базовый класс Контроллера фреймворка
+use app\widgets\currency\Currency;
+use ishop\App;
 
 class AppController extends Controller{
 
@@ -13,6 +13,10 @@ class AppController extends Controller{
         // перегрузка - переопределение методов и свойств родительского класса
         parent::__construct($route); // вызов родительского конструктора, чтобы его не затереть
         new AppModel(); // создаем объект базовой модели приложения
+        setcookie('currency', 'EUR', time() + 3600*24*7, '/');
+        App::$app->setProperty('currencies', Currency::getCurrencies());
+        App::$app->setProperty('currency', Currency::getCurrency(App::$app->getProperty('currencies')));
+        debug(App::$app->getProperties());
     }
 
 }
