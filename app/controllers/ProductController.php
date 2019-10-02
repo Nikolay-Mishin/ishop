f5d87cb0492a26d984392d75d24135b779437efb
Николай Мишин <mishin.nikolay.d3@mail.ru>
2019-10-02 18:42:34 +0300

    New files 2-15

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -22,8 +22,15 @@ class ProductController extends AppController {
         $related = \R::getAll("SELECT * FROM related_product JOIN product ON product.id = related_product.related_id WHERE related_product.product_id = ?", [$product->id]);
 
         // запись в куки запрошенного товара
+        $p_model = new Product();
+        $p_model->setRecentlyViewed($product->id);
 
         // просмотренные товары - последние 3 просмотренных товара
+        $r_viewed = $p_model->getRecentlyViewed();
+        $recentlyViewed = null;
+        if($r_viewed){
+            $recentlyViewed = \R::find('product', 'id IN (' . \R::genSlots($r_viewed) . ') LIMIT 3', $r_viewed);
+        }
 
         // галерея
         $gallery = \R::findAll('gallery', 'product_id = ?', [$product->id]);
@@ -31,7 +38,8 @@ class ProductController extends AppController {
         // модификации
 
         $this->setMeta($product->title, $product->description, $product->keywords);
-        $this->set(compact('product', 'related', 'gallery')); // передаем данные в вид карточки товара
+        // передаем данные в вид карточки товара
+        $this->set(compact('product', 'related', 'gallery', 'recentlyViewed'));
     }
 
 }
