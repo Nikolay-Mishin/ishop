d30093849ecf5853bede4f632a0bf97bd191b0a4
Николай Мишин <mishin.nikolay.d3@mail.ru>
2019-10-06 16:33:21 +0300

    New files 2-24

--- public/js/main.js
+++ public/js/main.js
@@ -31,25 +31,6 @@ $('#typeahead').bind('typeahead:select', function(ev, suggestion) {
 });
 /* // Search */
 
-// отслеживаем изменение выпадающего списка валют
-$('#currency').change(function(){
-    window.location = 'currency/change?curr=' + $(this).val(); // запрашиваем страницу и передаем управление контроллеру валюты
-});
-
-// отслеживаем изменение выпадающего списка модификаций
-$('.available select').on('change', function(){
-    var modId = $(this).val(), // идентификатор (id) выбранного модификатора
-        color = $(this).find('option').filter(':selected').data('title'), // цвет выбранного модификатора
-        price = $(this).find('option').filter(':selected').data('price'), // цена выбранного модификатора
-        basePrice = $('#base-price').data('base'); // базовая цена товара
-    // если цена установалена (выбран модификатор), устанавливаем цену модификатора
-    if(price){
-        $('#base-price').text(symboleLeft + price + symboleRight);
-    }else{
-        $('#base-price').text(symboleLeft + basePrice + symboleRight); // если пользователь вернулся к базовой версии товара
-    }
-});
-
 // корзина
 /* Cart */
 // событие при клике по сслыке для добавления в корзину
@@ -150,6 +131,26 @@ function clearCart() {
     }); */
 }
 /* // Cart */
+
+// отслеживаем изменение выпадающего списка валют
+$('#currency').change(function(){
+    window.location = 'currency/change?curr=' + $(this).val(); // запрашиваем страницу и передаем управление контроллеру валюты
+});
+
+// отслеживаем изменение выпадающего списка модификаций
+$('.available select').on('change', function(){
+    var modId = $(this).val(), // идентификатор (id) выбранного модификатора
+        color = $(this).find('option').filter(':selected').data('title'), // цвет выбранного модификатора
+        price = $(this).find('option').filter(':selected').data('price'), // цена выбранного модификатора
+        basePrice = $('#base-price').data('base'); // базовая цена товара
+    // если цена установалена (выбран модификатор), устанавливаем цену модификатора
+    if(price){
+        $('#base-price').text(symboleLeft + price + symboleRight);
+    }else{
+        $('#base-price').text(symboleLeft + basePrice + symboleRight); // если пользователь вернулся к базовой версии товара
+    }
+});
+
 
 // Ajax-запрос - отправляет стандартный ajax-запрос
 function ajax(url, success, data = {}, errorMsg = 'Ошибка! Попробуйте позже', type = 'GET') {
