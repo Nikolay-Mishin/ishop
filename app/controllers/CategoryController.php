918903c91201ddc4c2b0d7c3a40cbede2522f08a
Николай Мишин <mishin.nikolay.d3@mail.ru>
2019-10-07 19:35:17 +0300

    New files 2-26

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -5,10 +5,11 @@ namespace app\controllers;
 
 use app\models\Category; // модель категорий
 use app\models\Breadcrumbs; // модель хлебных крошек
+use ishop\App;
+use ishop\libs\Pagination; // класс пагинации (постраничной навигации)
 
 class CategoryController extends AppController {
 
-    // отображает вид страницы с товарами категории
     public function viewAction(){
         $alias = $this->route['alias']; // алиас запрошенной категории
         $category = \R::findOne('category', 'alias = ?', [$alias]); // получаем категорию из БД
@@ -25,11 +26,17 @@ class CategoryController extends AppController {
         // если вложенных категорий нет берем запрошенную категорию, иначе к вложенным категориям добавляем запрошенную
         $ids = !$ids ? $category->id : $ids . $category->id; // 4,6,7,5,8,9,10,1 (для категории 1)
 
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perpage = App::$app->getProperty('pagination');
+        $total = \R::count('product', "category_id IN ($ids)");
+        $pagination = new Pagination($page, $perpage, $total);
+        $start = $pagination->getStart();
+
         // товары списка полученных категория (IN ищет совпадение в заданном диапазоне - 4,6,7,5,8,9,10,1)
         // не биндим значение ("category_id IN ?", [$ids]), тк значение для выборки из БД мы формируем сами на основе данных из БД
-        $products = \R::find('product', "category_id IN ($ids)");
+        $products = \R::find('product', "category_id IN ($ids) LIMIT $start, $perpage");
         $this->setMeta($category->title, $category->description, $category->keywords);
-        $this->set(compact('products', 'breadcrumbs'));
+        $this->set(compact('products', 'breadcrumbs', 'pagination', 'total'));
     }
 
 }
