8bb069edea32212c28fa3366893a9167ea3e78fc
Николай Мишин <mishin.nikolay.d3@mail.ru>
2019-10-11 22:34:41 +0300

    Add comments 2-35

--- config/params.php
+++ config/params.php
@@ -5,6 +5,9 @@ return [
     'admin_email' => 'admin@mail.com',
     'shop_name' => 'Магазин ishop2.loc',
     'pagination' => 3,
-    'smtp_login' => '',
-    'smtp_password' => '',
+    'smtp_host' => 'smtp.ukr.net',
+    'smtp_port' => '2525',
+    'smtp_protocol' => 'ssl',
+    'smtp_login' => 'testishop2@ukr.net',
+    'smtp_password' => 'testishop_2',
 ];
