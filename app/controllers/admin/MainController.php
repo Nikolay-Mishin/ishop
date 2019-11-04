7294bd77a201e6feb8a15974d84efe75f09f0f26
Николай Мишин <mishin.nikolay.d3@mail.ru>
2019-11-04 17:02:10 +0300

    Add comments 2-3

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -7,10 +7,10 @@ namespace app\controllers\admin;
 class MainController extends AppController {
 
     public function indexAction(){
-        $countNewOrders = \R::count('order', "status = '0'");
-        $countUsers = \R::count('user');
-        $countProducts = \R::count('product');
-        $countCategories = \R::count('category');
+        $countNewOrders = \R::count('order', "status = '0'"); // получаем новые заказы
+        $countUsers = \R::count('user'); // получаем общее число зарегистрированных пользователей
+        $countProducts = \R::count('product'); // получаем общее число товаров
+        $countCategories = \R::count('category'); // получаем общее число категорий
         $this->setMeta('Панель управления'); // устанавливаем мета-данные
         $this->set(compact('countNewOrders', 'countCategories', 'countProducts', 'countUsers'));
     }
