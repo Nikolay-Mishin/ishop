d0ea315d1eb1044f150289167adebf8a462c4e2f
Николай Мишин <mishin.nikolay.d3@mail.ru>
2019-11-04 16:58:52 +0300

    New files 2-3

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -7,7 +7,12 @@ namespace app\controllers\admin;
 class MainController extends AppController {
 
     public function indexAction(){
+        $countNewOrders = \R::count('order', "status = '0'");
+        $countUsers = \R::count('user');
+        $countProducts = \R::count('product');
+        $countCategories = \R::count('category');
         $this->setMeta('Панель управления'); // устанавливаем мета-данные
+        $this->set(compact('countNewOrders', 'countCategories', 'countProducts', 'countUsers'));
     }
 
 }
