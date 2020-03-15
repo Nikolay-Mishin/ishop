42b1008158f439d998f87a6bcc17b35f6e4e5447
Николай Мишин <mishin.nikolay.d3@mail.ru>
2020-03-15 21:12:53 +0300

    Fix T_SetProperties

--- vendor/ishop/core/traits/T_setProperties.php
+++ vendor/ishop/core/traits/T_setProperties.php
@@ -17,31 +17,68 @@ trait T_SetProperties {
 		// если в свойствах класс существует ключ из переданных настроек, то заполняем данное свойство переданным значением
 		foreach($options as $k => $v){
 			// проверяем существет ли такое свойство у класса
-            //debug([$k => $v, 'isClosure' => $isClosure, 'condition' => $isClosure ? $condition($k) : $condition], 'property_exists' => property_exists($this, $k));
+			//debug([$k => $v, 'isClosure' => $isClosure, 'condition' => $isClosure ? $condition($k) : $condition], 'property_exists' => property_exists($this, $k));
 			if($isClosure ? $condition($k) : $condition){
 				$this->$k = $v;
 				$callback($k, $v);
 			}
 		}
-        //debug($this);
+		//debug($this);
 	}
 
 	protected function setArgs($condition, $callback){
-        $base_condition = function($k){ return property_exists($this, $k); };
+		$base_condition = function($k){ return property_exists($this, $k); };
+
+		$this->reverse([
+			[
+				'type' => 'true && null',
+				'condition' => true,
+				'callback' => null
+			],
+			[
+				'type' => 'true && function()',
+				'condition' => true,
+				'callback' => function(){}
+			],
+			[
+				'type' => 'true && false',
+				'condition' => true,
+				'callback' => false
+			],
+			[
+				'type' => 'function() && null',
+				'condition' => function(){},
+				'callback' => null
+			],
+			[
+				'type' => 'function() && function(){}',
+				'condition' => function(){},
+				'callback' => function(){}
+			],
+			[
+				'type' => 'function() && false',
+				'condition' => function(){},
+				'callback' => false
+			]
+		]);
+
 		$isClosure = $this->isClosure($condition);
-        //$noCallback = $callback === false;
-        $callback2 = null;
-        $callback2 = function(){};
-        //$callback2 = false;
-        $noCallback = $callback2 === false;
-        $reverse = $isClosure;
-        $reverse2 = $isClosure && !$noCallback;
-        debug(['class' => get_class($this), 'isClosure' => $isClosure, 'noCallback' => $noCallback, 'reverse' => $reverse2]);
-        if($reverse){
-            $callback = $isClosure ? $condition : $callback;
-            $condition = $isClosure ? $base_condition : $condition;
-        }
+		$reverse = $isClosure;
+		//debug(['class' => get_class($this), 'isClosure' => $isClosure, 'noCallback' => $noCallback, 'reverse' => $reverse]);
+		if($reverse){
+			$callback = $isClosure ? $condition : $callback;
+			$condition = $isClosure ? $base_condition : $condition;
+		}
 		return [$condition ?? $base_condition, $callback ?? function(){}, $isClosure];
+	}
+
+	private function reverse($array){
+		foreach($array as $item){
+			$isClosure = $this->isClosure($item['condition']);
+			$noCallback = !$this->isClosure($item['callback']) || $item['callback'] === false;
+			$reverse = $isClosure && $noCallback;
+			debug(['type' => $item['type'], 'isClosure' => $isClosure, 'callback' => $this->isClosure($item['callback']), 'noCallback' => $noCallback, 'reverse' => $reverse]);
+		}
 	}
 
 }
