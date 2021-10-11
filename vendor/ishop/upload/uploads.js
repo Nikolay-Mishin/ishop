5a5ba300120053c56fa60956f2d620275b525895
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-10-11 13:28:34 +0300

    Fix gitignore

--- vendor/ishop/upload/uploads.js
+++
@@ -1,6 +0,0 @@
-// Функция, выполняемая по окночании загрузки страницы
-$(document).ready (() => {
-    let uploads = [$('#file'), $('#file-2')];
-    let previews = [$('#image-preview-1'), $('#image-preview-2')];
-    preview_image (uploads, previews);
-});
